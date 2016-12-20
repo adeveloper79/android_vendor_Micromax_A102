0c6845aa4ed4cc2c424fb04233962af9239e2c37
aditya6572 <adityakumarteli7846@gmail.com>
2016-12-20 21:28:06 +0530

    Add files via upload

---
+++ A102-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/xbin,system/xbin)
+
